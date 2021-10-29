b2f8fa558b251790cc7b375718f063764cd6fd1f
Taylor Goldberg <46632678+tgoldberg36@users.noreply.github.com>
2021-10-28 20:08:22 -0400

    Setup App component and recieved initdata

--- src/comonents/App.js
+++ src/comonents/App.js
@@ -1,13 +1,36 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+import { handleInitialData } from '../actions/shared'
+import { BrowserRouter as Router, Route} from 'react-router-dom'
+import { connect } from 'react-redux'
+import LoadingBar from 'react-redux-loading'
+import Leaderboard from './Leaderboard'
 
 class App extends Component {
+  componentDidMount() {
+   this.props.dispatch(handleInitialData())
+  }
+
   render(){
     return (
-      <div>
-        Test
-      </div>
+      <Router>
+        <Fragment>
+          <LoadingBar />
+          {this.props.loading === true 
+            ? null
+            : <div>
+              <Route path='/' exact component={Leaderboard} />
+            </div>
+          }
+        </Fragment>
+      </Router>
     )
   }
 }
 
-export default App;
+function mapStateToProps({ loadingBar }) {
+  return {
+    loading: loadingBar > 0
+  }
+}
+
+export default connect(mapStateToProps)(App)
