9d3556fac62667fd6f098218a3f8208983960616
Taylor Goldberg <46632678+tgoldberg36@users.noreply.github.com>
2021-10-30 21:03:56 -0400

    added more content to dashboard

--- src/comonents/Dashboard.js
+++ src/comonents/Dashboard.js
@@ -1,16 +1,75 @@
 import React, { Component } from 'react'
-// import { connect } from 'react-redux'
+import { connect } from 'react-redux'
+import Question from './Question'
+import '../css/dashboard.css'
+
+function getUnansweredQuestions(questions, authedUser){
+  return (
+    questions.filter((q) => q.optionOne.votes.includes(authedUser) || q.optionTwo.votes.includes(authedUser))
+  )
+}
+
+function getAnsweredQuestions(questions, authedUser){
+  return (
+    questions.filter((q) => !q.optionOne.votes.includes(authedUser) && !q.optionTwo.votes.includes(authedUser))
+  )
+}
+
 
 class Dashboard extends Component {
+
+  state = {
+    questionCategory: 'Unanswered Questions',
+  }
+
+  handleQuestionCategory = e => {
+    const questionCategory = e.target.value
+
+    this.setState(() => ({
+      questionCategory
+    }))
+  }
+
   render() {
+    const { questionCategory } = this.state
+    const { authedUser, questions } = this.props
+
+    const answered = getUnansweredQuestions(Object.values(questions), authedUser)
+    const unanswered = getAnsweredQuestions(Object.values(questions), authedUser)
+
     return (
-      <div>
-        Dashboard
+      <div className={'dashboardContainer'}>
+        <div className={'btnContainer'}>
+          <button className={'dashBoardBtn'} onClick={this.handleQuestionCategory} value={'Unanswered Questions'}>Unanswered Questions</button>
+          <button className={'dashBoardBtn'} onClick={this.handleQuestionCategory} value={'Answered Questions'}>Answered Questions</button>
+        </div>
+        {console.log(answered, unanswered)}
+        { questionCategory === 'Unanswered Questions' && 
+          <ul className={'questionList'}>
+            { unanswered.map((q) => 
+            <li className={'question'}>
+              <Question id={q.id} />
+            </li>)}
+          </ul>}
+        { questionCategory === 'Answered Questions' && 
+          <ul className={'questionList'}>
+            { answered.map((q) => 
+            <li className={'question'}>
+              <Question id={q.id} />
+            </li>)}
+          </ul>}
       </div>
     )
   }
 }
 
 
+function mapStateToProps(state) {
+  const { authedUser, questions } = state
+  return {
+    authedUser,
+    questions,
+  }
+}
 
-export default Dashboard
+export default connect(mapStateToProps)(Dashboard)
---
+++ src/comonents/Question.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+class Question extends Component {
+  render() {
+    const { question, user, questionId } = this.props
+
+    return (
+      <div className={'questionContainer'}>
+        <div className={'header'}>
+          <h4>{`${user.name} asks...`}</h4>
+          <img src={user.avatarURL} alt="avatar!" className="avatar" />
+        </div>
+        <div className={'body'}>
+          <h5>Would you rather:</h5>
+            <p>A: {question.optionOne.text}</p>
+            <p>B: {question.optionTwo.text}</p>
+        </div>
+      </div>
+    )
+  }
+}
+
+
+function mapStateToProps({ questions, users }, { id }) {
+  const question = questions[id]
+  const questionId = question.id
+  const user = users[question.author]
+
+  return {
+    question,
+    questionId,
+    user
+  }
+}
+
+export default connect(mapStateToProps)(Question)
