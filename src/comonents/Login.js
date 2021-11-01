ddae46faebab3c08f9f7a846d58b4cdbc6190b81
Taylor Goldberg <46632678+tgoldberg36@users.noreply.github.com>
2021-10-31 20:01:30 -0400

    can now swap users

--- src/comonents/Login.js
+++ src/comonents/Login.js
@@ -1,33 +1,53 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { withRouter, Redirect } from "react-router-dom";
+import { setAuthedUser } from '../actions/authedUser'
 import '../css/login.css'
 
 class Login extends Component {
 
   state = {
     toHome: false,
-    id: null,
+    id: '',
   }
 
-  handleChange = (e) => {
-
+  setUser = (e) => {
+    console.log(e)
+    this.setState(() => ({
+      id: e.target.value
+    }))
   }
 
   handleSubmit = (e) => {
-    
+    e.preventDefault()
+
+    const { dispatch } = this.props
+
+    if(this.state.id !== ''){
+      dispatch(setAuthedUser(this.state.id))
+      this.setState({
+        toHome: true,
+      })
+    }else{
+      alert("Select A User")
+    }
   }
 
   render() {
     const { users } = this.props
+    const { toHome } = this.state
+
+    if (toHome) {
+      return <Redirect to={'/home'} />
+    }
 
     return (
       <div className={'loginContainer'}>
         <h2>Welcome to Would You Rather!</h2>
         <p>Select a user to login</p>
         <div className={'loginSelector'}>
-          <select >
-            <option disabled value="-1">
+          <select onChange={this.setUser}>
+            <option value={''}>
               Select a user...
             </option>
             {Object.keys(users).map((key) => (
@@ -36,7 +56,7 @@ class Login extends Component {
               </option>
             ))}
           </select>
-          <button>
+          <button onClick={this.handleSubmit} className={'loginBtn'}>
             Log-in
           </button>
         </div>
